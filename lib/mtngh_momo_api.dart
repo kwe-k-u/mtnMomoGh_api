5add18e812daef428ab15cebf11e534b93a31616
Kweku <kwekuappiah11@gmail.com>
2020-12-11 09:19:01 +0000

    Added code from Postman

--- lib/mtngh_momo_api.dart
+++ lib/mtngh_momo_api.dart
@@ -5,27 +5,17 @@ import 'package:http/http.dart' as http;
 
 
 class MTNGhMomo{
-  // String TOKEN_KEY = getTokenKey().then((value) => value);
+  String TOKEN_KEY; //TODO integrate with code
   static String SUBSCRIPTION_KEY = "6ce2769ab2e84acfa01bfd3b7f808bb3";
+
   //todo add user creation
 
   Future<Map<String,dynamic>> checkBalance(String number) async{
-    // var uri = Uri(
-    //   scheme: 'https',
-    //   host: 'sandbox.momodeveloper.mtn.com',
-    //   path: "collection/v1_0/account/balance",
-    //   userInfo: SUBSCRIPTION_KEY,
-    //   queryParameters: {
-    //     "Subscription Key" : SUBSCRIPTION_KEY
-    //   }
-    //   // fragment: 'baz',
-    //   // queryParameters: _yourQueryParameters,
-    // );
     http.Response data = await http.get("https://sandbox.momodeveloper.mtn.com/collection/v1_0/account/balance",
 
         headers: {
           "X-Target-Environment" : "sandbox",
-          // "Authorization" : "BASIC ${TOKEN_KEY}",
+          "Authorization" : "BASIC ${getAccessToken()}",
           "AccountHolderId" : "",
           "AccountHolderType" : "msisdn",
           "currency" :"GHC",
@@ -44,7 +34,7 @@ class MTNGhMomo{
 
 
 
-  Future<String> getTokenKey() async{
+  Future<String> getAccessToken() async{
     var request = http.Request('POST', Uri.parse('https://sandbox.momodeveloper.mtn.com/collection/token/'));
 
     request.headers.addAll({
@@ -94,6 +84,139 @@ class MTNGhMomo{
   }
 
   requestPayment() {
+  }
+
+
+  createUser() async{
+    var headers = {
+      'X-Reference-Id': 'fc6a3655-aca7-408d-a5db-2aa572a2c16d',
+      'Ocp-Apim-Subscription-Key': '6ce2769ab2e84acfa01bfd3b7f808bb3',
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6ImZjNmEzNjU1LWFjYTctNDA4ZC1hNWRiLTJhYTU3MmEyYzE2ZCIsImV4cGlyZXMiOiIyMDIwLTEyLTExVDEwOjAzOjUzLjE2OSIsInNlc3Npb25JZCI6ImMyNGRlZDY0LTM4ZDctNGZhYi1hZWNmLTIyNTI1Y2JmMDljNyJ9.jsLfOhXDPvslwvqZJWiEODni7Rb4DQ7Ff4H8rJytEzF8YweaIp2Dy46SqDJnRohWhJd8JHxLw_tOyoU-hFeEo_MEz5JQJXcxZO4gsexCA6JZgqxDLb3nUwGwY8NJ6Of74pA9hsmaBxnWH8m8v_CctraAWCOEdMC2Yq9hLtW7tlyYJdjzfDc-HUfle_HLNZb91Lmq4tyqZXVh6l_1w0fgmRdfZb16OBublONtlRL9ecch-bhGyJlLHWHUYhC4f5p-lHb4rL1bIw-PKSNfNa3GLlcC57Ix0_Jq80BjSVwICZO7hWHv7yWFAHij0oB7AwbfPUgjYXcKwBESYRFQKmsGzg',
+      'Content-Type': 'application/json'
+    };
+    var request = http.Request('POST', Uri.parse('https://sandbox.momodeveloper.mtn.com/v1_0/apiuser'));
+    request.body = '''{\r\n  "providerCallbackHost": "https://webhook.site/e4eb56d1-93a3-4be2-a01d-5ae7f43b2800"\r\n}''';
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    }
+    else {
+      print(response.reasonPhrase);
+    }
+
+  }
+
+  getUser() async{
+    var headers = {
+      'Ocp-Apim-Subscription-Key': '6ce2769ab2e84acfa01bfd3b7f808bb3',
+      '': '',
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6ImZjNmEzNjU1LWFjYTctNDA4ZC1hNWRiLTJhYTU3MmEyYzE2ZCIsImV4cGlyZXMiOiIyMDIwLTEyLTExVDEwOjAzOjUzLjE2OSIsInNlc3Npb25JZCI6ImMyNGRlZDY0LTM4ZDctNGZhYi1hZWNmLTIyNTI1Y2JmMDljNyJ9.jsLfOhXDPvslwvqZJWiEODni7Rb4DQ7Ff4H8rJytEzF8YweaIp2Dy46SqDJnRohWhJd8JHxLw_tOyoU-hFeEo_MEz5JQJXcxZO4gsexCA6JZgqxDLb3nUwGwY8NJ6Of74pA9hsmaBxnWH8m8v_CctraAWCOEdMC2Yq9hLtW7tlyYJdjzfDc-HUfle_HLNZb91Lmq4tyqZXVh6l_1w0fgmRdfZb16OBublONtlRL9ecch-bhGyJlLHWHUYhC4f5p-lHb4rL1bIw-PKSNfNa3GLlcC57Ix0_Jq80BjSVwICZO7hWHv7yWFAHij0oB7AwbfPUgjYXcKwBESYRFQKmsGzg'
+    };
+    var request = http.Request('GET', Uri.parse('https://sandbox.momodeveloper.mtn.com/v1_0/apiuser/fc6a3655-aca7-408d-a5db-2aa572a2c16d'));
+
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    }
+    else {
+    print(response.reasonPhrase);
+    }
+
+  }
+
+  getAPIKey() async{
+    var headers = {
+      'Ocp-Apim-Subscription-Key': '6ce2769ab2e84acfa01bfd3b7f808bb3',
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6ImZjNmEzNjU1LWFjYTctNDA4ZC1hNWRiLTJhYTU3MmEyYzE2ZCIsImV4cGlyZXMiOiIyMDIwLTEyLTExVDEwOjAzOjUzLjE2OSIsInNlc3Npb25JZCI6ImMyNGRlZDY0LTM4ZDctNGZhYi1hZWNmLTIyNTI1Y2JmMDljNyJ9.jsLfOhXDPvslwvqZJWiEODni7Rb4DQ7Ff4H8rJytEzF8YweaIp2Dy46SqDJnRohWhJd8JHxLw_tOyoU-hFeEo_MEz5JQJXcxZO4gsexCA6JZgqxDLb3nUwGwY8NJ6Of74pA9hsmaBxnWH8m8v_CctraAWCOEdMC2Yq9hLtW7tlyYJdjzfDc-HUfle_HLNZb91Lmq4tyqZXVh6l_1w0fgmRdfZb16OBublONtlRL9ecch-bhGyJlLHWHUYhC4f5p-lHb4rL1bIw-PKSNfNa3GLlcC57Ix0_Jq80BjSVwICZO7hWHv7yWFAHij0oB7AwbfPUgjYXcKwBESYRFQKmsGzg'
+    };
+    var request = http.Request('POST', Uri.parse('https://sandbox.momodeveloper.mtn.com/v1_0/apiuser/fc6a3655-aca7-408d-a5db-2aa572a2c16d/apikey?='));
+
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    }
+    else {
+      print(response.reasonPhrase);
+    }
+
+  }
+
+
+  requestToPay() async{
+    var headers = {
+      'X-Reference-Id': '30544527-62e9-4c7a-a8cf-dd690d3ac852',
+      'X-Target-Environment': 'sandbox',
+      'Ocp-Apim-Subscription-Key': '6ce2769ab2e84acfa01bfd3b7f808bb3',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6ImZjNmEzNjU1LWFjYTctNDA4ZC1hNWRiLTJhYTU3MmEyYzE2ZCIsImV4cGlyZXMiOiIyMDIwLTEyLTExVDEwOjAzOjUzLjE2OSIsInNlc3Npb25JZCI6ImMyNGRlZDY0LTM4ZDctNGZhYi1hZWNmLTIyNTI1Y2JmMDljNyJ9.jsLfOhXDPvslwvqZJWiEODni7Rb4DQ7Ff4H8rJytEzF8YweaIp2Dy46SqDJnRohWhJd8JHxLw_tOyoU-hFeEo_MEz5JQJXcxZO4gsexCA6JZgqxDLb3nUwGwY8NJ6Of74pA9hsmaBxnWH8m8v_CctraAWCOEdMC2Yq9hLtW7tlyYJdjzfDc-HUfle_HLNZb91Lmq4tyqZXVh6l_1w0fgmRdfZb16OBublONtlRL9ecch-bhGyJlLHWHUYhC4f5p-lHb4rL1bIw-PKSNfNa3GLlcC57Ix0_Jq80BjSVwICZO7hWHv7yWFAHij0oB7AwbfPUgjYXcKwBESYRFQKmsGzg'
+    };
+    var request = http.Request('POST', Uri.parse('https://sandbox.momodeveloper.mtn.com/collection/v1_0/requesttopay'));
+    request.body = '''{\r\n  "amount": "5.00",\r\n  "currency": "EUR",\r\n  "externalId": "rrf",\r\n  "payer": {\r\n    "partyIdType": "MSISDN",\r\n    "partyId": "0559582518"\r\n  },\r\n  "payerMessage": "momo api test",\r\n  "payeeNote": "pay note"\r\n}''';
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    }
+    else {
+    print(response.reasonPhrase);
+    }
+
+  }
+
+
+  getStatusOfToPay() async{
+    var headers = {
+      'X-Reference-Id': 'fc6a3655-aca7-408d-a5db-2aa572a2c16d',
+      'Ocp-Apim-Subscription-Key': '6ce2769ab2e84acfa01bfd3b7f808bb3',
+      'X-Target-Environment': 'sandbox',
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6ImZjNmEzNjU1LWFjYTctNDA4ZC1hNWRiLTJhYTU3MmEyYzE2ZCIsImV4cGlyZXMiOiIyMDIwLTEyLTExVDEwOjAzOjUzLjE2OSIsInNlc3Npb25JZCI6ImMyNGRlZDY0LTM4ZDctNGZhYi1hZWNmLTIyNTI1Y2JmMDljNyJ9.jsLfOhXDPvslwvqZJWiEODni7Rb4DQ7Ff4H8rJytEzF8YweaIp2Dy46SqDJnRohWhJd8JHxLw_tOyoU-hFeEo_MEz5JQJXcxZO4gsexCA6JZgqxDLb3nUwGwY8NJ6Of74pA9hsmaBxnWH8m8v_CctraAWCOEdMC2Yq9hLtW7tlyYJdjzfDc-HUfle_HLNZb91Lmq4tyqZXVh6l_1w0fgmRdfZb16OBublONtlRL9ecch-bhGyJlLHWHUYhC4f5p-lHb4rL1bIw-PKSNfNa3GLlcC57Ix0_Jq80BjSVwICZO7hWHv7yWFAHij0oB7AwbfPUgjYXcKwBESYRFQKmsGzg'
+    };
+    var request = http.Request('GET', Uri.parse('https://sandbox.momodeveloper.mtn.com/collection/v1_0/requesttopay/fc6a3655-aca7-408d-a5db-2aa572a2c16d'));
+
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    }
+    else {
+    print(response.reasonPhrase);
+    }
+
+  }
+
+
+  getAccountBalance() async{
+    var headers = {
+      'X-Target-Environment': 'sandbox',
+      'Ocp-Apim-Subscription-Key': '6ce2769ab2e84acfa01bfd3b7f808bb3',
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6ImZjNmEzNjU1LWFjYTctNDA4ZC1hNWRiLTJhYTU3MmEyYzE2ZCIsImV4cGlyZXMiOiIyMDIwLTEyLTExVDEwOjAzOjUzLjE2OSIsInNlc3Npb25JZCI6ImMyNGRlZDY0LTM4ZDctNGZhYi1hZWNmLTIyNTI1Y2JmMDljNyJ9.jsLfOhXDPvslwvqZJWiEODni7Rb4DQ7Ff4H8rJytEzF8YweaIp2Dy46SqDJnRohWhJd8JHxLw_tOyoU-hFeEo_MEz5JQJXcxZO4gsexCA6JZgqxDLb3nUwGwY8NJ6Of74pA9hsmaBxnWH8m8v_CctraAWCOEdMC2Yq9hLtW7tlyYJdjzfDc-HUfle_HLNZb91Lmq4tyqZXVh6l_1w0fgmRdfZb16OBublONtlRL9ecch-bhGyJlLHWHUYhC4f5p-lHb4rL1bIw-PKSNfNa3GLlcC57Ix0_Jq80BjSVwICZO7hWHv7yWFAHij0oB7AwbfPUgjYXcKwBESYRFQKmsGzg'
+    };
+    var request = http.Request('GET', Uri.parse('https://sandbox.momodeveloper.mtn.com/collection/v1_0/account/balance'));
+
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    }
+    else {
+      print(response.reasonPhrase);
+    }
+
   }
 
 
